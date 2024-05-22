bad7c54380f3b9fdd58754a5631894a63b9174d2
Maeyy23 <msmabel23@gmail.com>
2024-05-22 09:34:59 +0100

    bug fixes

--- src/seeding/index.js
+++ src/seeding/index.js
@@ -3,13 +3,13 @@ const Admin = require("../models/admin.model");
 
 const seedAdmin = async () => {
   const foundAdmin = await Admin.findOne({ role: "0" });
-  if (foundAdmin.length < 1) {
+  if (!foundAdmin) {
     const data = {
       firstName: "Aliu",
       lastName: "Omeiza",
       email: "mabel@techware.ng",
       phone: "0811231367",
-      password: await bcrypt.hash("testing", 10),
+      password: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
       role: "0",
     };
 
