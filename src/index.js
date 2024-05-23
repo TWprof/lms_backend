bdf4531509f3f5c3bc5a3fe4cf05edc2d1aba673
Maeyy23 <msmabel23@gmail.com>
2024-05-23 08:41:28 +0100

    cors error

--- src/index.js
+++ src/index.js
@@ -24,7 +24,11 @@ seedAdmin();
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/v1", userRoutes);
